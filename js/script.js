4816f3af24fd570291b05e5b554e271883e9f72e
V Kursinov <kursinov@mail.ru>
2022-04-16 21:04:24 +0300

    My fierst comit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const as = () => {
+ return console.log(88+"kkkhhhh");
+};
+
+// document.querySelector('p').textContent = 'asgjggjg';
+
+console.log(`Это функция ${as}`);
+// console.log(typeof 123n)
